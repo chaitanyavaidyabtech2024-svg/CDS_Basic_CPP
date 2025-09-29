8a961ab1774a12a3ed5721524e780fb8b77ff6b6
chaitanyavaidyabtech2024-svg <chaitanya.vaidya.btech2024@sitpune.edu.in>
2025-09-29 14:57:01 +0530

    Create Expt_1A.cpp

---
+++ Expt_1A.cpp
@@ -0,0 +1,18 @@
+// Name: Chaitanya Vaidya
+// PRN: 25070123502
+// Experiment 1(A)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+/* Output
+
+Hello, World!
+
+*/
